5540a484e257319d53bee27378966cb7e4ebea45
Daniel Ochoa <ochoastudio@gmail.com>
2018-04-13 09:46:42 -0700

    remove RT and refactor remove simple spam filter

--- sent_files/func.py
+++ sent_files/func.py
@@ -4,6 +4,9 @@ sys.path.insert(0, '../config')
 from twitter_config import api
 import tweepy
 import re
+# encoding=utf8  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 
 def get_user_timeline_tweets(usernames):
@@ -34,9 +37,15 @@ def recent_tweets(search_word, num_find = 100):
 		background_img = j.author.profile_use_background_image
 		screen_name = j.author.screen_name
 		profile_img = j.author.default_profile_image
+		tweet = j.text.encode('utf-8')
+		retweeted = j.retweeted
+
+		# print(j.retweeted)
 		#print(followers, favorites, statuses_count, background_img, profile_img, screen_name)
-		if followers > 20 and favorites > 20 and statuses_count > 20 and profile_img == False:
-			tweets.append(j.text)
+		if followers > 20 and favorites > 20 and statuses_count > 20 and profile_img == False and remove_spamy_tweets(tweet) == 0 and retweeted == False:
+			# if remove_spamy_tweets(tweet) == 0 and remove_retweets(tweet) == 0:
+			# print(tweet)
+			tweets.append(tweet)
 			count_added += 1
 		else:
 			count_removed += 1
@@ -49,12 +58,14 @@ def remove_spamy_tweets(tweet_string):
 	# when called check if return is 0 if not remove tweet.
 	vote = 0
 	tweet = str(tweet_string.lower())
-	spamy_phrases = ["special offer"]
-	for spammy_phrase in spamy_phrases:
+	spammy_phrases = ["special offer", "money making", "make money", "start earning", "earn bitcoin"]
+	for spammy_phrase in spammy_phrases:
 		found = re.findall('\\b' + spammy_phrase + '\\b', tweet)
 		if found:
-		    num_found -= 1
+		    vote += 1
+	print('spammy tweets removed', vote)
 	return vote
+
 
 
 
--- sent_files/twitter_sent.py
+++ sent_files/twitter_sent.py
@@ -8,31 +8,33 @@ from func import recent_tweets
 import time
 import mysql.connector
 import tweepy
+# encoding=utf8  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
-	
-museums = ['@legionofhonor', '@sfmoma', '@oaklandmuseumca', '@asianartmuseum', '@deyoungmuseum', '@MuseumModernArt', '@LACMA']
+#query_words = ["@sfmoma", "@oaklandmuseumca", "@asianartmuseum", "@MuseumModernArt", "@LACMA"]	
+query_words = ["bitcoin",'ethereum']
 
-for museum in museums:
+for q_word in query_words:
 	try:
-		tweet_arr = recent_tweets(museum, 100)
+		q_word = recent_tweets(q_word, 500)
 	except Exception as e:
 		print(str(e))
 	count = 0
 	gross_subjectivity = 0
 	gross_polarity = 0
 	try:
-		for tweet in tweet_arr[1]: 
+		for tweet in q_word[1]: 
 			blob = TextBlob(tweet)
 			for sentence in blob.sentences:
 				# print(sentence.sentiment)
 				gross_polarity += sentence.sentiment.polarity
 				gross_subjectivity +=sentence.sentiment.subjectivity
 				count += 1
-
 		avg_gp = gross_polarity/count * 100	
 		avg_gs = gross_subjectivity/count * 100
 
-		print([count, museum])
+		print([count, q_word[0]])
 		print([avg_gp, avg_gs ])
 		time.sleep(10)
 	except Exception as e:
@@ -40,8 +42,8 @@ for museum in museums:
 
 	try:
 		# enter results into desired DB.
-		print("Store data")
-		# db_insert_recent_tweets_sent(avg_gp, avg_gs,'recent_tweets', tweet_arr[0])
+		print("---")
+		# db_insert_recent_tweets_sent(avg_gp, avg_gs,'recent_tweets', q_word[0])
 	except Exception as e:
 		print(str(e))
 	
